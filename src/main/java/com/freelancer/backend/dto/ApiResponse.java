ade7596f0c30069a5ef781ed2488376eae1135da
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:17:09 +0200

    e por fim a api response de todo que esta em DTO

--- src/main/java/com/freelancer/backend/dto/ApiResponse.java
+++ src/main/java/com/freelancer/backend/dto/ApiResponse.java
@@ -1,4 +1,13 @@
 package com.freelancer.backend.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ApiResponse {
+    private Boolean success;
+    private String message;
 }
