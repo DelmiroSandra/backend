fc629a2586620db32ceab7a95ca30b1c3269a56a
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:16:00 +0200

    Configurando o login response

--- src/main/java/com/freelancer/backend/dto/LoginResponse.java
+++ src/main/java/com/freelancer/backend/dto/LoginResponse.java
@@ -1,4 +1,16 @@
 package com.freelancer.backend.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginResponse {
+    private String token;
+    private String tokenType = "Bearer";
+    private Long userId;
+    private String username;
+    private String email;
 }
