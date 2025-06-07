22294a78f6e31f2ace90089155b8e5b106631010
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:22:24 +0200

    configurando outro JWT para autenticacao

--- src/main/java/com/freelancer/backend/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/freelancer/backend/config/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,23 @@
 package com.freelancer.backend.config;
 
-public class JwtAuthenticationEntryPoint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+        // Quando usuário não autenticado tenta acessar recurso protegido
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Erro: Não autorizado");
+    }
 }
