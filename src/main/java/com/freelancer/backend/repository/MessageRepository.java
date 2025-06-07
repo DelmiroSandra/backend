bed463759898daa9bb300843f920e0b847e69f6a
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:13:10 +0200

    Configurando a funcao de mensagem onde usaremos user, mandar mensagem e receber

--- src/main/java/com/freelancer/backend/repository/MessageRepository.java
+++ src/main/java/com/freelancer/backend/repository/MessageRepository.java
@@ -1,4 +1,16 @@
 package com.freelancer.backend.repository;
 
-public class MessageRepository {
+import com.freelancer.backend.model.Message;
+import com.freelancer.backend.model.Project;
+import com.freelancer.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findBySenderAndReceiver(User sender, User receiver);
+    List<Message> findByProject(Project project);
+    List<Message> findByReceiver(User receiver);
 }
