144dbb7ff85cac7437ab7f642e6466ca8b4ce15c
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:12:10 +0200

    Configurando o repository aplication

--- src/main/java/com/freelancer/backend/repository/ApplicationRepository.java
+++ src/main/java/com/freelancer/backend/repository/ApplicationRepository.java
@@ -1,4 +1,17 @@
 package com.freelancer.backend.repository;
 
-public class ApplicationRepository {
+import com.freelancer.backend.model.Application;
+import com.freelancer.backend.model.Project;
+import com.freelancer.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    List<Application> findByProject(Project project);
+    List<Application> findByFreelancer(User freelancer);
+    Optional<Application> findByFreelancerAndProject(User freelancer, Project project);
 }
