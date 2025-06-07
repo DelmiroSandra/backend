03575362528fac15ad56da9670cb7b9e4043d904
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:13:44 +0200

    configurando review

--- src/main/java/com/freelancer/backend/repository/ReviewRepository.java
+++ src/main/java/com/freelancer/backend/repository/ReviewRepository.java
@@ -1,4 +1,16 @@
 package com.freelancer.backend.repository;
 
-public class ReviewRepository {
+import com.freelancer.backend.model.Project;
+import com.freelancer.backend.model.Review;
+import com.freelancer.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByReviewed(User reviewed);
+    List<Review> findByReviewer(User reviewer);
+    List<Review> findByProject(Project project);
 }
