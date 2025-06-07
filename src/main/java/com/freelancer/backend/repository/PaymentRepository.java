ac68543278230b327aee8f35c60fbce85f5b76fa
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:14:41 +0200

    Configurando o pagamente depois de tudo der certo

--- src/main/java/com/freelancer/backend/repository/PaymentRepository.java
+++ src/main/java/com/freelancer/backend/repository/PaymentRepository.java
@@ -1,4 +1,16 @@
 package com.freelancer.backend.repository;
 
-public class PaymentRepository {
+import com.freelancer.backend.model.Payment;
+import com.freelancer.backend.model.Project;
+import com.freelancer.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByPayer(User payer);
+    List<Payment> findByReceiver(User receiver);
+    List<Payment> findByProject(Project project);
 }
