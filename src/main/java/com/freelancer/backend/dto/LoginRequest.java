0a434026a96488d9ed7fd7d12d40534ce88db44b
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:15:28 +0200

    Configurando o login request

--- src/main/java/com/freelancer/backend/dto/LoginRequest.java
+++ src/main/java/com/freelancer/backend/dto/LoginRequest.java
@@ -1,4 +1,17 @@
 package com.freelancer.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
 }
