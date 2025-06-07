4d0ca97273d3b54bec099fbec94bf699da58d8cc
Delmiro Sandra <142592343+DelmiroSandra@users.noreply.github.com>
2025-06-08 00:11:38 +0200

    Configurando ProjectRepositorio

--- src/main/java/com/freelancer/backend/repository/ProjectRepository.java
+++ src/main/java/com/freelancer/backend/repository/ProjectRepository.java
@@ -1,4 +1,16 @@
 package com.freelancer.backend.repository;
 
-public class ProjectRepository {
+import com.freelancer.backend.model.Project;
+import com.freelancer.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    // Listar projetos abertos
+    List<Project> findByStatus(String status);
+    // Listar projetos de um contratante específico
+    List<Project> findByContractor(User contractor);
 }
